c656c9e4fb82edeeab214be25878918723d6324d
Saurabh Totey <SaurabhTotey@gmail.com>
2020-12-03 21:56:20 -0700

    Added RNG redundancy and fixed distanceBetween typo

--- src/labyrinth/mod.rs
+++ src/labyrinth/mod.rs
@@ -9,6 +9,7 @@ use std::cmp::min;
 const WIDTH: usize = 30;
 const HEIGHT: usize = 15;
 const MINIMUM_DISTANCE_BETWEEN_STARTING_POSITIONS: i32 = 10;
+const MAX_GENERATION_ATTEMPTS_BEFORE_RETRY: i32 = 1024;
 
 pub struct Labyrinth {
 	tiles: [[Tile; WIDTH]; HEIGHT], // all the tiles in the labyrinth
@@ -19,16 +20,14 @@ pub struct Labyrinth {
 impl Labyrinth {
 
 	/**
-	 * Creates a new labyrinth
+	 * Generates a list of tiles in a maze pattern
+	 * Only generates WALLs and WALKABLE tiles
 	 */
-	pub fn new() -> Labyrinth {
-		let mut rng = thread_rng();
+	fn generateMazeTiles<T: Rng>(mut rng: &mut T) -> [[Tile; WIDTH]; HEIGHT] {
 		let mut tiles = [[Tile::WALL; WIDTH]; HEIGHT];
 
 		let mut branchCandidates: Vec<(usize, usize)> = Vec::new();
 		branchCandidates.push((rng.gen_range(1, HEIGHT - 1), rng.gen_range(1, WIDTH - 1)));
-		let mut walkableTiles: Vec<(usize, usize)> = Vec::new();
-		walkableTiles.push(branchCandidates[0]);
 
 		while !branchCandidates.is_empty() {
 			let branchCandidate = branchCandidates.remove(rng.gen_range(0, branchCandidates.len()));
@@ -47,28 +46,53 @@ impl Labyrinth {
 			let location: (usize, usize) = *locationCandidates.choose(&mut rng).unwrap();
 			tiles[location.0][location.1] = Tile::WALKABLE;
 			branchCandidates.push(location);
-			walkableTiles.push(location);
 
 			if locationCandidates.len() > 1 {
 				branchCandidates.push(branchCandidate);
 			}
 		}
 
-		// TODO: may need to regenerate tiles if this is attempted too many times
-		// perhaps its possible that tiles has generated in such a way that we can never choose
-		// these locations
+		return tiles;
+	}
+
+	/**
+	 * Creates a new labyrinth
+	 */
+	pub fn new() -> Labyrinth {
+		let mut rng = thread_rng();
+
+		let mut tiles: [[Tile; WIDTH]; HEIGHT];
 		let mut exitLocation: (usize, usize) = (0, 0);
 		let mut minotaurLocation: (usize, usize) = (0, 0);
 		let mut heroLocation: (usize, usize) = (0, 0);
-		while min(min(Labyrinth::distanceBetweeen(tiles, exitLocation, minotaurLocation), Labyrinth::distanceBetweeen(tiles, exitLocation, heroLocation)), Labyrinth::distanceBetweeen(tiles, minotaurLocation, heroLocation)) < MINIMUM_DISTANCE_BETWEEN_STARTING_POSITIONS {
-			exitLocation = *walkableTiles.choose(&mut rng).unwrap();
-			minotaurLocation = *walkableTiles.choose(&mut rng).unwrap();
-			heroLocation = *walkableTiles.choose(&mut rng).unwrap();
-		}
 
-		tiles[exitLocation.0][exitLocation.1] = Tile::INVISIBLE_EXIT;
-		tiles[minotaurLocation.0][minotaurLocation.1] = Tile::MINOTAUR;
-		tiles[heroLocation.0][heroLocation.1] = Tile::HERO;
+		loop {
+			tiles = Labyrinth::generateMazeTiles(&mut rng);
+			let walkableTiles = (0 .. HEIGHT).into_iter().flat_map(|rowIndex|
+				(0 .. WIDTH).into_iter().map(move |colIndex|
+					(rowIndex, colIndex)
+				)
+			).filter(|location|
+				tiles[location.0][location.1] != Tile::WALL
+			).collect::<Vec<(usize, usize)>>();
+
+			let mut attempts = 0;
+			while attempts < MAX_GENERATION_ATTEMPTS_BEFORE_RETRY && min(min(Labyrinth::distanceBetweeen(tiles, exitLocation, minotaurLocation), Labyrinth::distanceBetweeen(tiles, exitLocation, heroLocation)), Labyrinth::distanceBetweeen(tiles, minotaurLocation, heroLocation)) < MINIMUM_DISTANCE_BETWEEN_STARTING_POSITIONS {
+				exitLocation = *walkableTiles.choose(&mut rng).unwrap();
+				minotaurLocation = *walkableTiles.choose(&mut rng).unwrap();
+				heroLocation = *walkableTiles.choose(&mut rng).unwrap();
+				attempts += 1;
+			}
+
+			if attempts >= MAX_GENERATION_ATTEMPTS_BEFORE_RETRY {
+				continue;
+			}
+
+			tiles[exitLocation.0][exitLocation.1] = Tile::INVISIBLE_EXIT;
+			tiles[minotaurLocation.0][minotaurLocation.1] = Tile::MINOTAUR;
+			tiles[heroLocation.0][heroLocation.1] = Tile::HERO;
+			break;
+		}
 
 		return Labyrinth { tiles, minotaurCoordinates: minotaurLocation, heroCoordinates: heroLocation, commonKnowledge: Vec::new() };
 	}
@@ -121,7 +145,7 @@ impl Labyrinth {
 
 		let mut minDistances = [[-1; WIDTH]; HEIGHT];
 		let mut currentLocations = vec![location0];
-		minDistances[location0.0][location1.1] = 0;
+		minDistances[location0.0][location0.1] = 0;
 
 		while minDistances[location1.0][location1.1] == -1 {
 			let mut newLocations: Vec<(usize, usize)> = Vec::new();
